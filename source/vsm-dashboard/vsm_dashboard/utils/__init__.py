9f17dcc5cbc60bc59af0bc712e77957fe5175521
jiang.feng <fengx.jiang@intel.com>
2014-11-25 11:13:00 +0800

    fix dashboard last update time
    Signed-off-by: jiang.feng <fengx.jiang@intel.com>

--- source/vsm-dashboard/vsm_dashboard/utils/__init__.py
+++ source/vsm-dashboard/vsm_dashboard/utils/__init__.py
@@ -21,8 +21,10 @@ def get_time_delta(dt):
 
         timedelta = datetime.datetime.now() - datetime.datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
         secs = timedelta.days * 3600 * 24 + timedelta.seconds
-        days = secs / (3600 * 24)
-        hours = (secs -days* 3600*24) / 3600
+        ab = 1 if secs > 0 else -1
+        days = abs(secs) / (3600 * 24) * ab
+        hours = (abs(secs) - abs(days) * 3600 * 24) / 3600 * ab
+
         if (hours >= 1) or (days >= 1):
             return str(days) + "days, " + str(hours) + " hours ago"
         minutes = secs / 60
@@ -36,8 +38,9 @@ def get_time_delta2(dt):
     try:
         timedelta = datetime.datetime.now() - datetime.datetime.strptime(dt,"%Y-%m-%d %H:%M:%S")
         secs = timedelta.days * 3600 * 24 + timedelta.seconds
-        days = secs / (3600 * 24)
-        hours = (secs -days* 3600*24) / 3600
+        ab = 1 if secs > 0 else -1
+        days = abs(secs) / (3600 * 24) * ab
+        hours = (abs(secs) - abs(days) * 3600 * 24) / 3600 * ab
         if (hours >= 1) or (days >= 1):
             return str(days) + "days, " + str(hours) + " hours"
         minutes = secs / 60
