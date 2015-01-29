6fbe3df0ca7c8021997238a18675ed7eb8d55cf1
Lu Yuan <yuan.y.lu@intel.com>
2015-01-29 23:14:49 +0800

    fix bug when add/remove/servers/monitors
    Signed-off-by: Lu Yuan <yuan.y.lu@intel.com>

--- source/vsm/vsm/scheduler/manager.py
+++ source/vsm/vsm/scheduler/manager.py
@@ -686,12 +686,19 @@ class SchedulerManager(manager.Manager):
             # It need to change the role defined in
             # server.manifest
             if ser['is_monitor'] == False:
-                if ser_ref['type'].find('monitor') != -1 and ser_ref['status'] == 'available':
+                if ser['is_storage'] == True and ser_ref['status'] == 'available':
                     values = {'type': 'storage'}
                     db.init_node_update(context, ser_ref['id'], values)
             if ser['is_monitor'] == True:
                 if ser_ref['type'].find('monitor') != -1 and ser_ref['status'] == 'Active':
                     ser['is_monitor'] = False
+                if ser['is_storage'] == False and ser_ref['status'] == 'available':
+                    values = {'type': 'monitor'}
+                    db.init_node_update(context, ser_ref['id'], values)
+                elif ser['is_storage'] == True and ser_ref['status'] == 'available':
+                    values = {'type': 'storage,monitor'}
+                    db.init_node_update(context, ser_ref['id'], values)
+                
 
         self._update_server_list_status(context, server_list, 'running')
         _update_ssh_key()
