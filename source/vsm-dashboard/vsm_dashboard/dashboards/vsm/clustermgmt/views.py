c631469086a8c07d122147d09c02e801b5b7e5b0
lorraine19871987 <lorraine19871987@gmail.com>
2015-03-06 12:51:05 +0800

    add button 'intergrate cluster' to dashboard

--- source/vsm-dashboard/vsm_dashboard/dashboards/vsm/clustermgmt/views.py
+++ source/vsm-dashboard/vsm_dashboard/dashboards/vsm/clustermgmt/views.py
@@ -27,7 +27,7 @@ from horizon import views
 from vsm_dashboard.api import vsm as vsmapi
 from .tables import ListServerTable
 from .tables import CreateClusterTable
-from .form import ShouanForm
+#from .form import ShouanForm
 from django.http import HttpResponse
 
 
