d037378b3a187fa9986d95a9932c820ed462c669
flyingfish007 <bxzhu_5355@163.com>
2015-07-07 16:09:36 +0800

    fix some UI problems in openstackconnect

--- source/vsm-dashboard/vsm_dashboard/dashboards/vsm/openstackconnect/views.py
+++ source/vsm-dashboard/vsm_dashboard/dashboards/vsm/openstackconnect/views.py
@@ -62,8 +62,7 @@ class IndexView(tables.DataTableView):
 
 class CreateView(forms.ModalFormView):
     form_class = AddOpenstackIPForm
-    template_name = 'vsm/flocking/openstackconnect.html'
-    success_url = reverse_lazy('horizon:vsm:openstackconnect:index')
+    template_name = 'vsm/openstackconnect/create.html'
 
 class UpdateView(forms.ModalFormView):
     form_class = UpdateOpenstackIPForm
