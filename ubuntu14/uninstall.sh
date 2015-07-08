dd3293b912f3020247125a62dcd27bb63ea8de68
ywang19 <yaguang.wang@intel.com>
2015-07-09 05:08:41 +0800

    add uninstall.sh for ubuntu14
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

---
+++ ubuntu14/uninstall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright 2014 Intel Corporation, All Rights Reserved.
+
+# Licensed under the Apache License, Version 2.0 (the"License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#  http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+#!/bin/bash
+
+TOPDIR=$(cd $(dirname "$0") && pwd)
+USER=`whoami`
+
+source $TOPDIR/hostrc
+
+for ip in $CONTROLLER_ADDRESS; do
+    ssh -t $ip "sudo clean-data -f; sudo apt-get remove -y ceph httpd librbd MariaDB-client MariaDB-devel MariaDB-server memcached openstack-keystone openstack-utils python-devel rabbitmq-server rbd-fuse vsm vsm-dashboard python-vsmclient vsm-deploy"
+done
+
+for ip in $AGENT_ADDRESS_LIST; do
+    ssh -t $ip "sudo clean-data -f; sudo apt-get remove -y erase ceph httpd librbd MariaDB-client MariaDB-devel MariaDB-server memcached openstack-keystone openstack-utils python-devel rabbitmq-server rbd-fuse vsm vsm-dashboard python-vsmclient vsm-deploy"
+done
