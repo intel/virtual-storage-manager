2e568f9673229def0b3893c4a5e103809bc6ad75
iansoft <shouanx.xu@intel.com>
2015-07-31 03:28:58 +0800

    yAxis moves &  set the cpu chart default chart

--- source/vsm-dashboard/static/dashboard/js/dashboard.js
+++ source/vsm-dashboard/static/dashboard/js/dashboard.js
@@ -35,7 +35,8 @@ require(
         cIOPs.setOption(GenerateLineOption());
         cLatency.setOption(GetLantencyOption());
         cBandwidth.setOption(GetBandwidthOption());
-        
+        cCPU.setOption(GenerateInitCPUOption());
+
     	//load Capacity
         loadCapacity();
         setInterval(function(){
@@ -562,10 +563,10 @@ function GenerateGaugeOption(value) {
 function GenerateLineOption(){
     var option = {
         grid :{
-            x:20,
-            y:20,
-            height:'80%',
-            width:'90%',
+            x:40,
+            y:55,
+            height:'60%',
+            width:'85%',
         },
         tooltip:{
             trigger:'axis'
@@ -630,10 +631,10 @@ function GenerateLineOption(){
 function GenerateCPUOption(legendList,seriesList){
     var option = {
         grid :{
-            x:20,
-            y:20,
-            height:'80%',
-            width:'90%',
+            x:40,
+            y:55,
+            height:'60%',
+            width:'85%',
         },
         tooltip:{
             trigger:'axis'
@@ -671,13 +672,66 @@ function GenerateCPUOption(legendList,seriesList){
     return option;
 }
 
+function GenerateInitCPUOption(){
+    var option = {
+        grid :{
+            x:40,
+            y:55,
+            height:'60%',
+            width:'85%',
+        },
+        tooltip:{
+            trigger:'axis'
+        },
+        legend:{
+            data:[""]
+        },
+        xAxis : [
+            {
+                type : 'category',
+                boundaryGap : false,
+                axisLabel:{
+                    show:true,
+                    interval:0,
+                },
+                data :InitAxis_X()
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value',
+                scale: false,
+                min:0,
+                //max:15,
+                axisLabel:{
+                    show:true,
+                    interval:'auto',
+                    formatter: '{value}'
+                },
+                name : '',
+                boundaryGap: [0.5, 0.5]
+            }
+        ],
+        series : [
+            {
+                name:'--',
+                type:'line',
+                smooth:true,
+                itemStyle: {normal: {areaStyle: {type: 'default'}}},
+                data:InitValues(0)
+            },
+        ]
+    };
+    return option;
+}
+
 function GetLantencyOption(){
     option = {
         grid :{
-            x:20,
-            y:20,
-            height:'80%',
-            width:'90%',
+            x:40,
+            y:55,
+            height:'60%',
+            width:'85%',
         },
         tooltip:{
             trigger:'axis'
@@ -746,10 +800,10 @@ function GetLantencyOption(){
 function GetBandwidthOption(){
     option = {
         grid :{
-            x:20,
-            y:20,
-            height:'80%',
-            width:'90%',
+            x:40,
+            y:55,
+            height:'60%',
+            width:'85%',
         },
         tooltip:{
             trigger:'axis'
