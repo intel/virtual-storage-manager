18d0085d4e3650b69a1ff7f05e550027393042b9
ywang19 <yaguang.wang@intel.com>
2015-08-19 11:11:20 +0800

    fixed the bug that show the spin when run the ajax request
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

--- source/vsm-dashboard/static/dashboard/js/vsm.spin.js
+++ source/vsm-dashboard/static/dashboard/js/vsm.spin.js
@@ -25,11 +25,13 @@ var spinner = new Spinner(spin_opts);
 
 function GenerateSpin(){
 	var html = "";
+		html += "<div class='modal-dialog' style='height:100px;width:150px;background-color:white;top:250px'>";
 		html += "	<div class='modal-content spin-content'>";
 		html += "		<div class='spin-body'>";
 		html += "			<div id='divSpin'></div>";
 		html += "		</div>"
 		html += "	</div>";
+		html += "</div>";
 
 
 		var dialogWrapper = $("#modal_wrapper")[0];
