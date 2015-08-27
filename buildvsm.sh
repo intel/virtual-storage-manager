4632a319483afc405ca405ece141a874e2257440
ywang19 <yaguang.wang@intel.com>
2015-08-28 00:15:04 +0800

    add build number into Version at packaging
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

--- buildvsm.sh
+++ buildvsm.sh
@@ -59,8 +59,8 @@ TOPDIR=$(cd $(dirname "$0") && pwd)
 TEMP=`mktemp`; rm -rfv $TEMP >/dev/null; mkdir -p $TEMP;
 #DATE=`date "+%Y%m%d"`
 
-sed -i "s,Version: *.*,Version: $VERSION,g" $TOPDIR/source/python-vsmclient/PKG-INFO
-sed -i "s,Version: *.*,Version: $VERSION,g" $TOPDIR/source/vsm/PKG-INFO
+sed -i "s,Version: *.*,Version: $BUILD,g" $TOPDIR/source/python-vsmclient/PKG-INFO
+sed -i "s,Version: *.*,Version: $BUILD,g" $TOPDIR/source/vsm/PKG-INFO
 
 is_lsb_release=0
 lsb_release -a >/dev/null 2>&1 && is_lsb_release=1
