34830da55401618002bb184e9172d11840e04181
ywang19 <yaguang.wang@intel.com>
2015-09-28 15:33:28 +0800

    keep major dependent packages
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

--- ubuntu14/uninstall.sh
+++ ubuntu14/uninstall.sh
@@ -24,16 +24,10 @@ USER=`whoami`
 source $TOPDIR/installrc
 
 for ip in $CONTROLLER_ADDRESS; do
-    ssh -t $ip "sudo clean-data -f; sudo service vsm-api stop; sudo service vsm-scheduler stop; sudo service vsm-conductor stop; sudo service mysql stop; sudo service rabbitmq-server stop; sleep 3; sudo apt-get -y remove ceph httpd MariaDB-client MariaDB-server memcached rabbitmq-server openstack-keystone rbd-fuse vsm vsm-dashboard python-vsmclient vsm-deploy; sudo apt-get -y autoclean; sudo apt-get -y autoremove"
+    ssh -t $ip "sudo clean-data -f; sudo service vsm-api stop; sudo service vsm-scheduler stop; sudo service vsm-conductor stop; sudo service mysql restart; sudo service rabbitmq-server restart; sleep 3; sudo apt-get remove --yes vsm vsm-dashboard python-vsmclient vsm-deploy; sudo apt-get autoclean --yes"
 done
 
-sudo apt-get purge mysql* mariadb*
-sudo rm -fr /var/lib/mysql /etc/mysql /var/log/mysql ~/.mysql*
-sudo apt-get purge rabbitmq-server
-sudo killall beam
-sudo rm -fr /var/lib/rabbitmq /etc/rabbitmq /var/log/rabbitmq
-
 for ip in $AGENT_ADDRESS_LIST; do
-    ssh -t $ip "sudo clean-data -f; sudo service vsm-agent stop; sudo service vsm-physical stop; sudo apt-get -y remove ceph httpd librbd MariaDB-client MariaDB-devel MariaDB-server memcached openstack-keystone openstack-utils python-devel rabbitmq-server rbd-fuse vsm vsm-dashboard python-vsmclient vsm-deploy; sudo apt-get -y autoclean; sudo apt-get -y autoremove"
+    ssh -t $ip "sudo clean-data -f; sudo service vsm-agent stop; sudo service vsm-physical stop; sudo apt-get remove --yes vsm vsm-dashboard python-vsmclient vsm-deploy; sudo apt-get autoclean --yes"
 done
 
