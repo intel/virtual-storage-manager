876eb95f3541ecbdb1937747c2f9552df40b66ae
lorraine19871987 <lorraine19871987@gmail.com>
2015-10-30 08:58:41 +0800

    show disk name in add new osd UI

--- source/vsm-dashboard/vsm_dashboard/dashboards/vsm/devices-management/views.py
+++ source/vsm-dashboard/vsm_dashboard/dashboards/vsm/devices-management/views.py
@@ -139,7 +139,7 @@ class AddOSDView(TemplateView):
                 "result_mode":"get_disks",
             })
             disks = ret['disks'] or []
-            disks_list = [disk for disk in disks if disk.find('by-path') != -1]
+            disks_list = [disk for disk in disks if disk.find('by-path') == -1]
             context["available_disks"] = disks_list
         
         return context
