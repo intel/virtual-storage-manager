1cb3901b8b68f7f38e3e47a4c1e02196593ae351
iansoft <anziloveanzi@hotmail.com>
2015-11-10 14:26:39 +0800

    change the import cluster UI and add the validate function

--- source/vsm-dashboard/static/dashboard/js/importcluster.js
+++ source/vsm-dashboard/static/dashboard/js/importcluster.js
@@ -1,12 +1,14 @@
-
 $(function(){
 	console.log("Import Cluster Ready");
 
-	InitCtrlCSS();
+	initCtrlCSS();
+
+	//init the dialog
+	atuoDetectDialog("Auto Detect",$("#divAutoDetectInfo")[0].innerHTML);
+	$("#divAutoDetectInfo").remove();
 });
 
-
-function InitCtrlCSS(){
+function initCtrlCSS(){
 	var ctrlSelect = $("select");
 	for(var i=0;i<ctrlSelect.length;i++){
 		ctrlSelect[i].className = "form-control";
@@ -18,41 +20,85 @@ function InitCtrlCSS(){
 	}
 }
 
-$("#btnImportCluster").click(function(){
-	if(   $("#id_monitor_host").val() == ""
-	   || $("#id_monitor_keyring").val() == ""
-	   || $("#id_cluster_conf").val() == ""){
-		showTip("error","The field is marked as '*' should not be empty");
+
+function atuoDetectDialog(title,body){
+	var html = "";
+	html += "<div class='modal-dialog' style='z-index:1045'>";
+	html += "	<div class='modal-content' >";
+	html += "		<div class='modal-header'>";
+	html += "			<button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button>";
+	html += "			<h4 class='modal-title' id='myModalLabel'>";
+	html += title;
+	html += "			</h4>";
+	html += "		</div>";
+	html += "		<div class='modal-body'>";
+	html += body;
+	html += "		</div>";
+	html += "		<div class='modal-footer'>";
+	html += "			<button id='btnDialogSubmit' type='button' class='btn btn-primary' onclick='addDetect()'>Auto Detect</button>";
+	html += "			<button id='btnDialogCancel' type='button' class='btn btn-default' data-dismiss='modal'>Close</button>";
+	html += "		</div>";
+	html += "	</div>";
+	html += "</div>";
+
+	var dialogWrapper = $("#modal_wrapper")[0];
+	dialogWrapper.id = "modal_wrapper";
+	dialogWrapper.className = "modal fade";
+	dialogWrapper.innerHTML = html;
+
+	return $("#modal_wrapper");
+}
+
+function openAddDetectDialog(){
+	$("#modal_wrapper").modal("show");
+	//$("#btnDialogCancel").hide();
+	return false;
+}
+
+function addDetect(){
+	//Check the field is should not null
+	if($("#txtMonitorKeyring").val() == ""){
+		showTip("error","Monitor Keyring should not be empty");
 		return  false;
 	}
 
-	var monitor_host_id = $("#id_monitor_host").val();
-	var monitor_host_name = $("#id_monitor_host")[0].options[$("#id_monitor_host")[0].selectedIndex].text;
-	var monitor_keyring = $("#id_monitor_keyring").val();
-	var cluster_conf = $("#id_cluster_conf").val();
+	var ctrlServerList = $("#selMonitorHost")[0];
+	var monitor_id = ctrlServerList.value;
+	var monitor_name = ctrlServerList.options[ctrlServerList.selectedIndex].text;
+	var keyring = $("#txtMonitorKeyring").val();
+
+	//set the value for submit
+	$("#selMonitorHost2")[0].selectedIndex = ctrlServerList.selectedIndex;
+	$("#txtMonitorKeyring2").val($("#txtMonitorKeyring").val());
+
 
 	var data = {
-		"monitor_host_id":monitor_host_id,
-		"monitor_host_name":monitor_host_name,
-		"monitor_keyring":monitor_keyring,
-		"cluster_conf":cluster_conf,
+		"monitor_id":monitor_id,
+		"monitor_name":monitor_name,
+		"keyring":keyring,
 	}
 
 	var postData = JSON.stringify(data);
 	token = $("input[name=csrfmiddlewaretoken]").val();
 	$.ajax({
 		type: "post",
-		url: "/dashboard/vsm/cluster-import/import_cluster/",
+		url: "/dashboard/vsm/cluster-import/auto_detect/",
 		data: postData,
 		dataType:"json",
 		success: function(data){
-                if (data.status=='Failed') {
-                     showTip("error",data.message)
-                }
-                else{
-                    showTip("success",data.message)
-                }
-				window.location.href="/dashboard/vsm/cluster-import/";
+				$("#txtCrushmap")[0].innerHTML += data.message;
+				
+				//clear the controls
+				ctrlServerList.selectedIndex = 0;
+				$("#txtMonitorKeyring").val("")
+				$("#modal_wrapper").modal("hide");
+				$(".messages").empty();
+
+				//show the error
+				if(data.status == "Failed"){
+					showTip("error",data.message);
+					return  false;
+				}
 		   	},
 		error: function (XMLHttpRequest, textStatus, errorThrown) {
 				if(XMLHttpRequest.status == 500)
@@ -65,4 +111,109 @@ $("#btnImportCluster").click(function(){
 
 		}
     });
-})
+}
+
+
+
+function validate(){
+	//Check the field is should not null
+	if(    $("#txtCrushmap").val() == ""
+	 	|| $("#txtCephConf").val() == ""){
+		showTip("error","Configuration should not be empty");
+		return  false;
+	}
+
+	var data = {
+		"crushmap":$("#txtCrushmap")[0].value,
+		"cephconf":$("#txtCephConf")[0].value,
+	}
+
+	var postData = JSON.stringify(data);
+	token = $("input[name=csrfmiddlewaretoken]").val();
+	$.ajax({
+		type: "post",
+		url: "/dashboard/vsm/cluster-import/validate_conf/",
+		data: postData,
+		dataType:"json",
+		success: function(data){
+				switch(data.status){
+					case "OK":
+						$("#btnAutoDetect").hide();
+						$("#btnImportValidate").hide();
+						$("#btnImportCluster").show();
+						$("#divMonitorHost2").show();
+						$("#divtxtMonitorKeyring2").show();
+						showTip("success",data.message);
+						break;
+					case "Failed":
+						showTip("error",data.message);
+						break;
+				}
+		   	},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+				if(XMLHttpRequest.status == 500)
+                	showTip("error","INTERNAL SERVER ERROR")
+			},
+		headers: {
+			"X-CSRFToken": token
+			},
+		complete: function(){
+
+		}
+    });
+}
+
+function importCluster(){
+	//Check the field is should not null
+	if(   $("#selMonitorHost2").val() == ""
+	   || $("#txtMonitorKeyring2").val() == ""
+	   || $("#txtCrushmap")[0].value  == ""
+	   || $("#txtCephConf")[0].value  == ""){
+		showTip("error","The field is marked as '*' should not be empty");
+		return  false;
+	}
+
+	var monitor_host_id = $("#selMonitorHost2").val();
+	var monitor_host_name = $("#selMonitorHost2")[0].options[$("#selMonitorHost2")[0].selectedIndex].text;
+	var monitor_keyring = $("#txtMonitorKeyring2").val();
+	var crushmap = $("#txtCrushmap").val();
+	var cephconf = $("#txtCephConf").val();
+
+	var data = {
+		"monitor_host_id":monitor_host_id,
+		"monitor_host_name":monitor_host_name,
+		"monitor_keyring":monitor_keyring,
+		"crushmap":crushmap,
+		"cephconf":cephconf,
+	}
+
+	var postData = JSON.stringify(data);
+	token = $("input[name=csrfmiddlewaretoken]").val();
+	$.ajax({
+		type: "post",
+		url: "/dashboard/vsm/cluster-import/import_cluster/",
+		data: postData,
+		dataType:"json",
+		success: function(data){
+			switch(data.status){
+				case "OK":
+					showTip("success",data.message);
+					break;
+				case "Failed":
+					showTip("error",data.message);
+					break;
+		   	}
+		   	//window.location.href= "/dashboard/vsm/cluster-import/";
+		},
+		error: function (XMLHttpRequest, textStatus, errorThrown) {
+				if(XMLHttpRequest.status == 500)
+                	showTip("error","INTERNAL SERVER ERROR")
+			},
+		headers: {
+			"X-CSRFToken": token
+			},
+		complete: function(){
+
+		}
+    });
+}
