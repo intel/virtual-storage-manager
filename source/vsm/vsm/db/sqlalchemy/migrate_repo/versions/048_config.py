78e3c8c4beea05d4b3690874262972c4c59cf518
ywang19 <yaguang.wang@intel.com>
2015-12-08 16:33:10 +0800

    add config table to save configurable items
    Signed-off-by: ywang19 <yaguang.wang@intel.com>

---
+++ source/vsm/vsm/db/sqlalchemy/migrate_repo/versions/048_config.py
@@ -0,0 +1,56 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# Copyright 2014 Intel Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from sqlalchemy import Boolean, Column, DateTime, Text
+from sqlalchemy import Integer, MetaData, String, Table
+
+def upgrade(migrate_engine):
+    # Upgrade operations go here. Don't create your own engine;
+    # bind migrate_engine to your metadata
+    meta = MetaData()
+    meta.bind = migrate_engine
+
+    config = Table(
+        'config', meta,
+        Column('id', Integer, primary_key=True, nullable=False),
+        Column('name', String(length=255), nullable=False),
+        Column('value', String(length=255), nullable=True),
+        Column('default_value', String(length=255), nullable=False),
+        Column('category', String(length=255), nullable=True),
+        Column('section', String(length=255), nullable=False),
+        Column('description', String(length=255), nullable=True),
+        Column('alterable', Boolean(create_constraint=True, name=None)),
+        Column('created_at', DateTime(timezone=False)),
+        Column('updated_at', DateTime(timezone=False)),
+        Column('deleted_at', DateTime(timezone=False)),
+        Column('deleted', Boolean(create_constraint=True, name=None)),
+    )
+
+    try:
+        config.create()
+    except Exception:
+        meta.drop_all(tables=[config])
+        raise
+
+def downgrade(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+
+    configs = Table('config',
+                    meta,
+                    autoload=True)
+    configs.drop()
