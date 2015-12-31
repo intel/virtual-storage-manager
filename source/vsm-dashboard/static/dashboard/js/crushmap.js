d01b828f35bee817affc8219e49d5422528a0f1c
lorraine19871987 <lorraine19871987@gmail.com>
2015-12-31 15:25:26 +0800

    fix bug:crusmap ui reset error

--- source/vsm-dashboard/static/dashboard/js/crushmap.js
+++ source/vsm-dashboard/static/dashboard/js/crushmap.js
@@ -156,8 +156,17 @@ function ResetForm(){
 	$("#txtFriendlyName").val("");
 	$("#txtMarker").val("");
 	$("#tTakeList>tbody").empty();
-	//$("#btnAddStorageGroup").hide();
-	//$("#btnUpdateStorageGroup").hide();
+
+    if(_SG_ID==null){
+        $("#btnAddStorageGroup").show();
+        $("#btnUpdateStorageGroup").hide();
+    }
+    else {
+        $("#btnAddStorageGroup").hide();
+        $("#btnUpdateStorageGroup").show();
+    }
+
+
 }
 
 function CancelAction(){
