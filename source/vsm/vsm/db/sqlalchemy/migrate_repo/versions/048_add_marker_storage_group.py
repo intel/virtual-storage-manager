a4f13a6a8ba31385ba882b727920ddccede6f9ba
root <root@sample.com>
2016-01-19 11:04:57 +0800

    fix a column name typo
    Signed-off-by: root <root@sample.com>

--- source/vsm/vsm/db/sqlalchemy/migrate_repo/versions/048_add_marker_storage_group.py
+++ source/vsm/vsm/db/sqlalchemy/migrate_repo/versions/048_add_marker_storage_group.py
@@ -36,7 +36,7 @@ def downgrade(migrate_engine):
     meta.bind = migrate_engine
 
     storage_group = Table('storage_groups', meta, autoload=True)
-    marker = Column('take_id',String(length=255), nullable=True)
+    marker = Column('marker',String(length=255), nullable=True)
 
     try:
         storage_group.drop_column(marker)
