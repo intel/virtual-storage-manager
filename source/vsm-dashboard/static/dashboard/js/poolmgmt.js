9075c4802c96d1b6b580e3963b143a58de802593
iansoft <anziloveanzi@hotmail.com>
2016-01-19 16:59:26 -0800

    remove the unuseful code

--- source/vsm-dashboard/static/dashboard/js/poolmgmt.js
+++ source/vsm-dashboard/static/dashboard/js/poolmgmt.js
@@ -48,77 +48,6 @@ $("#id_enable_pool_quota").click(function(){
         $("#id_pool_quota")[0].readOnly = true;
 });
 
-
-
-$("#btnAddCacheTier").click(function(){
-	//Check the field is should not null
-	if(   $("#id_hit_set_count").val() == ""
-	   || $("#id_hit_set_period_s").val() == ""
-	   || $("#id_target_max_mem_mb").val() == ""
-	   || $("#id_target_dirty_ratio").val() == ""
-	   || $("#id_target_full_ratio").val() == ""
-	   || $("#id_target_max_objects").val() == ""
-	   || $("#id_target_min_flush_age_m").val() == ""
-	   || $("#id_target_min_evict_age_m").val() == "" ){
-
-		showTip("error","The field is marked as '*' should not be empty");
-		return  false;
-	}
-
-
-	var CachePoolID = $("#id_cache_tier_pool").val();
-	var StoragePoolID = $("#id_storage_tier_pool").val();
-
-	if(CachePoolID == StoragePoolID){
-		showTip("error","Failed to add cache tier: cache_pool, storage_pool cannot be the same!")
-		return  false;
-	}
-
-	var data = {
-		"cache_tier":{
-			"storage_pool_id":StoragePoolID,
-			"cache_pool_id":CachePoolID,
-			"cache_mode":$("#id_cache_mode").val(),
-			'force_nonempty': $("#id_force_nonempty")[0].checked,
-			"options":{
-				"hit_set_type":$("#id_hit_set_type").val(),
-				'hit_set_count':$("#id_hit_set_count").val(),
-				'hit_set_period_s':$("#id_hit_set_period_s").val(),
-				'target_max_mem_mb':$("#id_target_max_mem_mb").val(),
-				'target_dirty_ratio':$("#id_target_dirty_ratio").val(),
-				'target_full_ratio':$("#id_target_full_ratio").val(),
-				'target_max_objects':$("#id_target_max_objects").val(),
-				'target_min_flush_age_m':$("#id_target_min_flush_age_m").val(),
-				'target_min_evict_age_m':$("#id_target_min_evict_age_m").val(),
-			}
-		}
-	}
-
-	var postData = JSON.stringify(data);
-	token = $("input[name=csrfmiddlewaretoken]").val();
-	console.log(token)
-	$.ajax({
-		type: "post",
-		url: "/dashboard/vsm/poolsmanagement/add_cache_tier_action/",
-		data: postData,
-		dataType:"json",
-		success: function(data){
-				console.log(data);
-				window.location.href="/dashboard/vsm/poolsmanagement/";
-		   	},
-		error: function (XMLHttpRequest, textStatus, errorThrown) {
-				if(XMLHttpRequest.status == 500)
-                	showTip("error","INTERNAL SERVER ERROR")
-			},
-		headers: {
-			"X-CSRFToken": token
-			},
-		complete: function(){
-
-		}
-    });
-})
-
 $("#btnRemoveCacheTier").click(function(){
 	var CachePoolID = $("#id_cache_tier_pool").val();
 	var data = {
@@ -154,59 +83,6 @@ $("#btnRemoveCacheTier").click(function(){
 })
 
 
-$("#btnAddReplicatedPool").click(function(){
-	//Check the field is should not null
-	if(   $("#id_name").val() == ""
-	   || $("#id_replication_factor").val() == ""
-	   || $("#id_max_pg_num_per_osd").val() == ""
-	   || $("#id_tag").val() == ""
-	   || $("#id_pool_quota").val() == "" ){
-
-		showTip("error","The field is marked as '*' should not be empty");
-		return  false;
-	}
-
-
-	var data = {
-			"pool":{
-				"name":$("#id_name").val(),
-				"storageGroupId":$("#id_storage_group").val(),
-                'storageGroupName': $("#id_storage_group")[0].options[$("#id_storage_group")[0].selectedIndex].text,
-				"replicatedStorageGroupId":$("#id_replicated_storage_group").val(),
-				'replicationFactor': $("#id_replication_factor").val(),
-				'max_pg_num_per_osd': $("#id_max_pg_num_per_osd").val(),
-                'auto_growth_pg': $("#id_auto_growth_pg").val(),
-				'tag': $("#id_tag").val(),
-				'clusterId': "0",
-				'createdBy': "VSM",
-				'enablePoolQuota': $("#id_enable_pool_quota")[0].checked,
-				'poolQuota': $("#id_pool_quota").val(),
-			}
-	}
-
-	var postData = JSON.stringify(data);
-    token = $("input[name=csrfmiddlewaretoken]").val();
-	$.ajax({
-		type: "post",
-		url: "/dashboard/vsm/poolsmanagement/create_replicated_pool_action/",
-		data: postData,
-		dataType:"json",
-		success: function(data){
-				console.log(data);
-				window.location.href="/dashboard/vsm/poolsmanagement/";
-		   	},
-		error: function (XMLHttpRequest, textStatus, errorThrown) {
-				if(XMLHttpRequest.status == 500)
-                	showTip("error","INTERNAL SERVER ERROR")
-			},
-		headers: {
-			"X-CSRFToken": token
-			},
-		complete: function(){
-
-		}
-    });
-});
 
 $("#btnCreateErasureCodedPool").click(function(){
 	//Check the field is should not null
