289b91b6b5a78c55da0025c7234c5e754436fb8c
flyingfish007 <bxzhu_5355@163.com>
2016-05-11 14:43:27 +0800

    remove the pagination from user page to setting page

--- source/vsm-dashboard/vsm_dashboard/dashboards/vsm/settings/views.py
+++ source/vsm-dashboard/vsm_dashboard/dashboards/vsm/settings/views.py
@@ -14,30 +14,50 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import logging
+from datetime import datetime
 import json
+import logging
+
+from horizon.utils import functions as utils
 
 from django.http import HttpResponse
+from django import shortcuts
 from django.views.generic import TemplateView
+
 from vsm_dashboard.api import vsm as vsmapi
 
 LOG = logging.getLogger(__name__)
 
+
 class IndexView(TemplateView):
+
     template_name = 'vsm/settings/index.html'
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
-        settings = [{
-            'name': "storage_group_near_full_threshold",
-            'value': 80,
-            }]
         settings = vsmapi.get_settings(self.request)
         for setting in settings:
             setting.verbose_name = setting.name.upper()
-        context['settings'] = [x for x in settings if
-                               x.verbose_name in ("STORAGE_GROUP_NEAR_FULL_THRESHOLD",
-                                                  "STORAGE_GROUP_FULL_THRESHOLD","DISK_NEAR_FULL_THRESHOLD","DISK_FULL_THRESHOLD",'CPU_DIAMOND_COLLECT_INTERVAL','CEPH_DIAMOND_COLLECT_INTERVAL','KEEP_PERFORMANCE_DATA_DAYS')]
+        pagination_value = utils.get_page_size(self.request)
+        pagination_setting = {"name": "pagination",
+                              "verbose_name": "PAGINATION",
+                              "value": pagination_value}
+        setting_name_tuple = ("STORAGE_GROUP_NEAR_FULL_THRESHOLD",
+                              "STORAGE_GROUP_FULL_THRESHOLD",
+                              "DISK_NEAR_FULL_THRESHOLD",
+                              "DISK_FULL_THRESHOLD",
+                              "CPU_DIAMOND_COLLECT_INTERVAL",
+                              "CEPH_DIAMOND_COLLECT_INTERVAL",
+                              "KEEP_PERFORMANCE_DATA_DAYS")
+        context['settings'] = [x for x in settings
+                               if x.verbose_name in setting_name_tuple]
+        context['settings'].append(pagination_setting)
         return context
+
+def _one_year():
+    now = datetime.utcnow()
+    return datetime(now.year + 1, now.month, now.day, now.hour,
+                    now.minute, now.second, now.microsecond, now.tzinfo)
 
 def SettingsAction(request, action):
     data = json.loads(request.body)
@@ -47,6 +67,17 @@ def SettingsAction(request, action):
         msg = "No server selected"
     else:
         if action == "update":
+            key_name = data['keyName']
+            if key_name == 'pagination':
+                response = shortcuts.redirect(request.build_absolute_uri())
+                request.session['horizon_pagesize'] = data['keyValue']
+                response.set_cookie('horizon_pagesize', data['keyValue'],
+                                    expires=_one_year())
+                status = "success"
+                msg = "Update Success"
+                resp = dict(message=msg, status=status, data="")
+                resp = json.dumps(resp)
+                return HttpResponse(resp)
             #vsmapi.add_servers(request, data)
             try:
                 setting_value = int(data['keyValue'])
