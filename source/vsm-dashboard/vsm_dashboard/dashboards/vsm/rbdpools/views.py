4585db4959beaddd7fef510cfaef550bebd29183
flyingfish007 <bxzhu_5355@163.com>
2016-06-03 11:39:11 +0800

    fix issues of supporting ceph as glance store

--- source/vsm-dashboard/vsm_dashboard/dashboards/vsm/rbdpools/views.py
+++ source/vsm-dashboard/vsm_dashboard/dashboards/vsm/rbdpools/views.py
@@ -142,14 +142,16 @@ def PoolsAction(request, action):
         resp = json.dumps(resp)
         return HttpResponse(resp)
 
-    pool_usages = vsmapi.pool_usages(request)
-    for pool_usage in pool_usages:
-        if pool_usage['as_glance_store_pool']:
-            msg = "there is one pool as glance backend now"
-            status = "error"
-            resp = dict(message=msg, status=status, data="")
-            resp = json.dumps(resp)
-            return HttpResponse(resp)
+    if as_glance_store_pool_num:
+        pool_usages = vsmapi.pool_usages(request)
+        for pool_usage in pool_usages:
+            print pool_usage.as_glance_store_pool
+            if pool_usage.as_glance_store_pool:
+                msg = "there is one pool as glance backend now"
+                status = "error"
+                resp = dict(message=msg, status=status, data="")
+                resp = json.dumps(resp)
+                return HttpResponse(resp)
 
     if not len(data):
         status = "error"
@@ -162,7 +164,8 @@ def PoolsAction(request, action):
             pools = []
             for x in data:
                 cinder_volume_host = x['cinder_volume_host']
-                if (not cinder_volume_host) and (not x['as_glance_store_pool']):
+                if (cinder_volume_host == "" or cinder_volume_host == None) and \
+                        (x['as_glance_store_pool'] == "" or x['as_glance_store_pool'] == None):
                     status = "error"
                     msg = "The Cinder Volume Host and As Glance Store Pool are all null"
                 pools.append({'pool_id': x['id'],
